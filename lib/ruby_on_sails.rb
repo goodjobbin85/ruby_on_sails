87bdf39ba1f34edc359d694b934292d32f763292
Thomas Dubiel <td1729@hotmail.com>
2021-10-06 13:51:26 -0500

    create Application class with call method in RubyOnSails module

--- lib/ruby_on_sails.rb
+++ lib/ruby_on_sails.rb
@@ -3,6 +3,9 @@
 require_relative "ruby_on_sails/version"
 
 module RubyOnSails
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application 
+    def call(env) 
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Sails"]]
+    end 
+  end 
 end
